430507ba14a576a96a56fc5c69c852114db61d42
EdisonnTran <80595547+EdisonnTran@users.noreply.github.com>
2022-03-30 19:48:10 -0400

    test saving (it's not working for me)

---
+++ src/main/java/com/ftw/ftwmagic/ftwmagic.java
@@ -0,0 +1,11 @@
+package com.ftw.ftwmagic;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ftwmagic implements ModInitializer {
+    public static final String MOD_ID = "ftwmagic";
+    @Override
+    public void onInitialize() {
+
+    }
+}
